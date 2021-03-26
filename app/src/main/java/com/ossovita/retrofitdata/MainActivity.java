b99a7c1cf9a85c20c458261b45711d0709883190
yasineryigit <yasineryigit737@gmail.com>
2021-03-26 15:48:23 +0300

    getPhotosByAlbumId & getPhotoById, showDataOnRecycler  methods added

--- app/src/main/java/com/ossovita/retrofitdata/MainActivity.java
+++ app/src/main/java/com/ossovita/retrofitdata/MainActivity.java
@@ -40,22 +40,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.recycler_view1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
         recyclerView.setHasFixedSize(true);
         Retrofit retrofit = new Retrofit.Builder()
                                 .baseUrl("https://jsonplaceholder.typicode.com/")
                                 .addConverterFactory(GsonConverterFactory.create())
                                 .build();
         photosApi = retrofit.create(PhotosApi.class);
-        getPhotosByAlbumId();//resimleri çekip listeyi dolduracak
-        //photos.add(new Photos(12,15,"New Photo","https://via.placeholder.com/150/25474f"));
-
-
+        getPhotosByAlbumId(15);//resimleri çekip listeyi dolduracak
+        //getPhotoById(40);
     }
 
 
-    private void getPhotosByAlbumId() {
+    private void getPhotosByAlbumId(int albumId) {
 
-        Call<List<Photos>> call = photosApi.getPhotosByAlbumId(15);//gelen verileri Photos türünde obje listesine dönüştürecek.
+        Call<List<Photos>> call = photosApi.getPhotosByAlbumId(albumId);//gelen verileri Photos türünde obje listesine dönüştürecek.
 
         call.enqueue(new Callback<List<Photos>>() {
             @Override
@@ -66,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 //hata yoksa komple gelen body'i obje listemize at
                 photos = response.body();//recyclerView listemizi güncelledik
-                showPhotos();
+                showDataOnRecycler(photos);
             }
 
             @Override
@@ -77,10 +76,37 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void showPhotos() {
+    private void getPhotoById(int photoId) {
+
+        Call<List<Photos>> call = photosApi.getPhotosById(photoId);//gelen verileri Photos türünde obje listesine dönüştürecek.
+
+        call.enqueue(new Callback<List<Photos>>() {
+            @Override
+            public void onResponse(Call<List<Photos>> call, Response<List<Photos>> response) {
+                if(!response.isSuccessful()){//gelen veride hata varsa hata kodunu yaz
+                    Toast.makeText(MainActivity.this, "Error code: \" + response.code()", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //hata yoksa komple gelen body'i obje listemize at
+                photos = response.body();//recyclerView listemizi güncelledik
+                showDataOnRecycler(photos);
+            }
+
+            @Override
+            public void onFailure(Call<List<Photos>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Error: "+ t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+
+    private void showDataOnRecycler(List<Photos> photos) {
         adapter = new PhotoAdapter(photos);
         recyclerView.setAdapter(adapter);//listeyi alıp recyclerı güncelleyecek
-        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
         adapter.notifyDataSetChanged();//listeyi güncellediğimizi adapter'a söylüyoruz
     }
+
+
 }
